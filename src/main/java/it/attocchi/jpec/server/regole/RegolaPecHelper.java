6060de8c6ff7afe1e0ae6b245e909cdc78056f7a
Mirco <amirco@gmail.com>
2015-12-18 09:04:41 +0100

    aggiornato regole validazione importazione

--- src/main/java/it/attocchi/jpec/server/regole/RegolaPecHelper.java
+++ src/main/java/it/attocchi/jpec/server/regole/RegolaPecHelper.java
@@ -1,7 +1,15 @@
 package it.attocchi.jpec.server.regole;
 
 import it.attocchi.jpec.server.bl.RegolaPecBL;
+import it.attocchi.jpec.server.entities.RegolaPec;
+import it.attocchi.mail.parts.MailAttachmentUtil;
+import it.attocchi.mail.utils.MailUtils;
 
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.mail.Address;
 import javax.mail.Flags.Flag;
 import javax.mail.Message;
 
@@ -15,21 +23,108 @@ import org.slf4j.LoggerFactory;
  */
 public class RegolaPecHelper {
 
-	protected static final Logger logger = LoggerFactory.getLogger(RegolaPecBL.class);
-	
+	protected final Logger logger = LoggerFactory.getLogger(RegolaPecBL.class);
+
+	private Message messaggioEmail;
+	private RegolaPec regola;
+
+	public RegolaPecHelper(RegolaPec regola, Message messaggioEmail) {
+		super();
+		this.messaggioEmail = messaggioEmail;
+		this.regola = regola;
+	}
+
 	/**
 	 * 
-	 * @param email
+	 * @param messaggioEmail
 	 * @return
 	 */
-	public boolean isMessaggioDaLeggere(Message email) {
+	public boolean isMessaggioDaLeggere() {
 		boolean res = false;
 		try {
-			res = !email.getFlags().contains(Flag.SEEN);
+			logger.debug("criterio isMessaggioDaLeggere applicato da regola {} su messaggio {}", regola, messaggioEmail);
+			res = !messaggioEmail.getFlags().contains(Flag.SEEN);
+		} catch (Exception ex) {
+			logger.error("errore isMessaggioDaLeggere", ex);
+		}
+		return res;
+	}
+
+	/**
+	 * 
+	 * @param regExp
+	 * @return
+	 */
+	public boolean mittenteMatch(String regExp) {
+		boolean res = false;
+		try {
+			logger.debug("criterio mittenteMatch applicato da regola {} su messaggio {}", regola, messaggioEmail);
+			Address[] senders = messaggioEmail.getFrom();
+			if (senders != null) {
+				for (Address sender : senders) {
+					Pattern pattern = Pattern.compile(regExp);
+					Matcher matcher = pattern.matcher(sender.toString());
+					res = matcher.matches();
+					if (res) {
+						break;
+					}
+				}
+			}
 		} catch (Exception ex) {
 			logger.error("errore isMessaggioDaLeggere", ex);
 		}
 		return res;
 	}
 	
+	/**
+	 * 
+	 * @param regExp
+	 * @return
+	 */
+	public boolean destinatarioMatch(String regExp) {
+		boolean res = false;
+		try {
+			logger.debug("criterio destinatarioMatch applicato da regola {} su messaggio {}", regola, messaggioEmail);
+			Address[] recipients = messaggioEmail.getAllRecipients();
+			if (recipients != null) {
+				for (Address recipient : recipients) {
+					Pattern pattern = Pattern.compile(regExp);
+					Matcher matcher = pattern.matcher(recipient.toString());
+					res = matcher.matches();
+					if (res) {
+						break;
+					}
+				}
+			}
+		} catch (Exception ex) {
+			logger.error("errore isMessaggioDaLeggere", ex);
+		}
+		return res;
+	}
+	
+	/**
+	 * 
+	 * @param regExp
+	 * @return
+	 */
+	public boolean attachmentNameMatch(String regExp) {
+		boolean res = false;
+		try {
+			logger.debug("criterio attachmentNameMatch applicato da regola {} su messaggio {}", regola, messaggioEmail);
+			List<MailAttachmentUtil> attachments = MailUtils.getAttachments(messaggioEmail);
+			if (attachments != null) {
+				for (MailAttachmentUtil attachment : attachments) {
+					Pattern pattern = Pattern.compile(regExp);
+					Matcher matcher = pattern.matcher(attachment.getFileName());
+					res = matcher.matches();
+					if (res) {
+						break;
+					}
+				}
+			}
+		} catch (Exception ex) {
+			logger.error("errore isMessaggioDaLeggere", ex);
+		}
+		return res;
+	}	
 }
