85328d611ee0da18114146d63880a3507f09468f
Mirco <amirco@gmail.com>
2015-12-30 19:59:41 +0100

    aggiunto servizio rest per risorsa messaggi

---
+++ src/main/java/it/attocchi/jpec/server/api/rest/ResourceMessaggi.java
@@ -0,0 +1,96 @@
+package it.attocchi.jpec.server.api.rest;
+
+import it.attocchi.jpec.server.api.rest.data.NuovoMessaggioRequest;
+import it.attocchi.jpec.server.api.rest.data.NuovoMessaggioResponse;
+import it.attocchi.jpec.server.bl.MessaggioPecBL;
+import it.attocchi.jpec.server.entities.MessaggioPec;
+import it.attocchi.jpec.server.exceptions.PecException;
+import it.webappcommon.rest.RestBaseJpa2;
+
+import java.net.URI;
+import java.util.Date;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Link;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.Gson;
+
+@Path("/messaggi")
+public class ResourceMessaggi extends RestBaseJpa2 {
+
+	protected static final Logger logger = LoggerFactory.getLogger(ResourceMessaggi.class);
+
+	@GET
+	@Path("/{id}/")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getMessaggio() {
+		Response response = null;
+		try {
+			logger.debug("{}", restServletContext.getContextPath());
+			response = Response.ok(new Date().toString(), MediaType.TEXT_PLAIN).build();
+		} catch (Exception ex) {
+			logger.error("doRicevi", ex);
+			response = Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).type(MediaType.TEXT_PLAIN).build();
+		}
+		return response;
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response nuovo(NuovoMessaggioRequest requestData) {
+		Response response = null;
+		NuovoMessaggioResponse responseData = new NuovoMessaggioResponse();
+		try {
+			logger.debug("{}", restServletContext.getContextPath());
+			logger.debug("{}", new Gson().toJson(requestData));
+
+			MessaggioPec messaggioCreato = MessaggioPecBL.creaMessaggio(getContextEmf(), requestData, "REST.ANONYMOUS");
+
+			// MessaggioPecBL.inviaEmail(getContextEmf(), null, null,
+			// "REST.ANONYMOUS");
+
+			responseData.setMessageId(messaggioCreato.getId());
+			URI uri = UriBuilder.fromUri(uriInfo.getAbsolutePath()).path("{id}").build(responseData.getMessageId());
+			// URI uri =
+			// URI.create("http://stackoverflow.com/questions/24968448");
+			// Link link = Link.fromUri(uri).rel("self").build();
+			Link link = Link.fromUri(uri).build();
+			responseData.setLink(link);
+
+			response = Response.ok(responseData).build();
+
+			// URI messageUri = new URI("/test/" + new Date().getTime());
+			// response = Response.created(messageUri).build();
+
+			// throw new
+			// NotImplementedException("questa api non e' ancora disponibile");
+			// } catch (AuthorizeException ex) {
+			// logger.error("UNAUTHORIZED", ex);
+			// response =
+			// Response.status(Response.Status.UNAUTHORIZED).entity(ex.getMessage()).type(MediaType.TEXT_PLAIN).build();
+			// } catch (AS400UnavailableException ex) {
+			// logger.error("SERVICE_UNAVAILABLE", ex);
+			// response =
+			// Response.status(Response.Status.SERVICE_UNAVAILABLE).entity(ex.getMessage()).type(MediaType.TEXT_PLAIN).build();
+		} catch (PecException ex) {
+			logger.error("PRECONDITION_FAILED", ex);
+			response = Response.status(Response.Status.PRECONDITION_FAILED).entity(ex.getMessage()).type(MediaType.TEXT_PLAIN).build();
+		} catch (Exception ex) {
+			logger.error("INTERNAL_SERVER_ERROR", ex);
+			response = Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).type(MediaType.TEXT_PLAIN).build();
+		}
+		return response;
+	}
+
+}
