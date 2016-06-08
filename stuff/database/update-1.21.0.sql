6978333c9226f471264bf1a30174a1473d6508b8
Mirco <amirco@gmail.com>
2016-06-08 15:54:44 +0200

    aggiunto note cambio filtro e verifica database

--- stuff/database/update-1.21.0.sql
+++ stuff/database/update-1.21.0.sql
@@ -1,2 +1,7 @@
 
 ALTER TABLE jpec.pec01_messaggi ADD COLUMN pec01_segnatura_xml longtext;
+
+-- cambia il filtro delle ricevute soloRicevuteConRiferimento quindi cercare quante sono:
+-- xRicevuta  ma senza xRiferimentoMessageID
+select pec01_x_ricevuta, pec01_x_riferimento_message_id from pec01_messaggi;
+select * from pec01_messaggi WHERE pec01_x_ricevuta <> '' and (pec01_x_riferimento_message_id is null or pec01_x_riferimento_message_id = '');
