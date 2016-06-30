74cc0b3699b39916691a4bc071035334dea3022f
Mirco <amirco@gmail.com>
2016-06-30 19:52:19 +0200

    aggiunti test per helper regole

---
+++ src/test/java/TestRegolaHelper.java
@@ -0,0 +1,44 @@
+import it.attocchi.jpec.server.entities.RegolaPec;
+import it.attocchi.jpec.server.regole.RegolaPecHelper;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import javax.mail.Message;
+import javax.mail.internet.MimeMessage;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestRegolaHelper {
+
+	@Test
+	public void testAllegatoEccezioneXml() throws Exception {
+		RegolaPec regolaTest = new RegolaPec();
+		regolaTest.setNome("TEST");
+		InputStream is = new FileInputStream("/home/mirco/Desktop/RicevutaEccezione.eml");
+		Message mime = new MimeMessage(null, is);
+		RegolaPecHelper helper = new RegolaPecHelper(regolaTest, mime);
+		Assert.assertTrue(helper.attachmentNameMatch("Eccezione.xml"));
+	}
+	
+	@Test
+	public void testAllegatoPostacertXml() throws Exception {
+		RegolaPec regolaTest = new RegolaPec();
+		regolaTest.setNome("TEST");
+		InputStream is = new FileInputStream("/home/mirco/Desktop/RicevutaEccezione.eml");
+		Message mime = new MimeMessage(null, is);
+		RegolaPecHelper helper = new RegolaPecHelper(regolaTest, mime);
+		Assert.assertTrue(helper.attachmentNameMatch("postacert.eml"));
+	}
+	
+	@Test
+	public void testAllegatoSenzaEccezioneXml() throws Exception {
+		RegolaPec regolaTest = new RegolaPec();
+		regolaTest.setNome("TEST");
+		InputStream is = new FileInputStream("/home/mirco/Desktop/RicevutaEccezione.eml");
+		Message mime = new MimeMessage(null, is);
+		RegolaPecHelper helper = new RegolaPecHelper(regolaTest, mime);
+		Assert.assertFalse(helper.attachmentNameMatch("TEEST.xml"));
+	}
+}
