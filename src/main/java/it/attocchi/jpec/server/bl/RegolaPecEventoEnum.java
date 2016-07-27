cebcac2daaddb326cbf30425fbef7fb9509f4a40
Mirco <amirco@gmail.com>
2016-07-27 10:40:41 +0200

    format codice

--- src/main/java/it/attocchi/jpec/server/bl/RegolaPecEventoEnum.java
+++ src/main/java/it/attocchi/jpec/server/bl/RegolaPecEventoEnum.java
@@ -1,11 +1,11 @@
 package it.attocchi.jpec.server.bl;
 
 public enum RegolaPecEventoEnum {
-	
+
 	IMPORTA_MESSAGGIO,
 	PROTOCOLLA_MESSAGGIO,
 	AGGIORNA_STATO,
 	AGGIORNA_SEGNATURA,
 	NOTIFICA
-	
+
 }
